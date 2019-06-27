081cc9f2338e4e9cd84f3b3db74fc963c88c6c01
Antoine Bonnin <bonnin.annto@gmail.com>
2019-06-27 11:12:45 +0200

    robustify measures by performing averages for each US and dropping too high values

--- sigfox_device/sigfox_device.ino
+++ sigfox_device/sigfox_device.ino
@@ -1,6 +1,7 @@
  
 #include <SigFox.h>
 #include <ArduinoLowPower.h>
+#include <math.h>
 
 // defines
 #define DEBUG 1
@@ -17,6 +18,9 @@
 #define MIN_LEVEL 0
 #define MAX_LEVEL 100
 #define LEVEL_FULL_SCALE (MAX_LEVEL - MIN_LEVEL)
+
+#define RETRY_PER_US 6 // For each US, measure RETRY_PER_US times and average
+#define DROP_COEF 1.3 // Measures for a US that are > (mean of measures for the US * DROP_COEF) are dropped
 
 
 // defines variables
@@ -43,7 +47,7 @@ void reboot() {
 
 void setup() {
 
-  delay(10000); // This 10s delay allows you to reprog the Arduino before it goes in LowPower.sleep() :)
+  delay(25000); // This 25s delay allows you to reprog the Arduino before it goes in LowPower.sleep() :)
 
   for (int i =0; i < us_num; i++) { 
     pinMode(trigPin[i], OUTPUT); // Sets the trigPin as an Output
@@ -72,27 +76,73 @@ void loop() {
 
   level = 0;
   weight_factor_sum = 0;
+  for (int a = 0; a < us_num; ++a)
+    distance[a] = 0;
   
   for (int i =0; i < us_num; i++) {
-    delayMicroseconds(30000);  // 0.0mM3 sec between each measure to make sure no rebounds of previous sensor occurs
-    
-    // Clears the trigPins
-    digitalWrite(trigPin[i], LOW);
-    Serial.print("US "); Serial.print(i); Serial.print("  -->  ");
-    Serial.print("Pins "); Serial.print(trigPin[i]); Serial.print(" - ");Serial.print(echoPin[i]); Serial.print("  ///  ");
-    delayMicroseconds(2);
-    
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin[i], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[i], LOW);
-    
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[i], HIGH);
-    
-    // Calculating the distance
-    distance[i] = duration*0.034/2;
+    Serial.println(" "); Serial.print("US "); Serial.print(i); Serial.print("  -->  ");
+    Serial.print("Pins "); Serial.print(trigPin[i]); Serial.print(" - ");Serial.print(echoPin[i]); Serial.println("  ///  ");
 
+    int tmp_distance[RETRY_PER_US];
+    for (int b = 0; b < RETRY_PER_US; ++b)
+      distance[b] = 0;    
+    
+    for (int j =0; j < RETRY_PER_US; j++) {
+      delayMicroseconds(50000);  // 50milisec between each measure to make sure no rebounds of previous sensor occurs
+      
+      // Clears the trigPins
+      digitalWrite(trigPin[i], LOW);
+      delayMicroseconds(2);
+      
+      // Sets the trigPin on HIGH state for 10 micro seconds
+      digitalWrite(trigPin[i], HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin[i], LOW);
+      
+      // Reads the echoPin, returns the sound wave travel time in microseconds
+      duration = pulseIn(echoPin[i], HIGH);
+      
+      // Calculating the distance
+      tmp_distance[j] = duration*0.034/2;
+      if (DEBUG){
+        Serial.print("    US #");
+        Serial.print(i);
+        Serial.print("- tmp Distance for measure #");
+        Serial.print(j);        
+        Serial.print(": ");
+        Serial.println(tmp_distance[j]);
+        delay(1);
+      }      
+    }
+
+    // Filter tmp distance for the US and drop too high
+    // Compute mean and drop measures above mean * DROP_COEF
+
+    // Mean
+    int us_mean=0;
+    for (int id=0; id<RETRY_PER_US; id++){
+      us_mean += tmp_distance[id]; 
+    }
+    us_mean /= RETRY_PER_US;
+    Serial.print("us_mean: ");Serial.println(us_mean);
+    
+
+    // Drop too big measures for this US and compute average of the remaining measures
+    int divider = 0;
+    for (int idx=0; idx<RETRY_PER_US; idx++){
+      if (tmp_distance[idx] <= us_mean * DROP_COEF) {
+        distance[i] += tmp_distance[idx];
+        divider += 1;
+      }
+      else {
+        if (DEBUG){
+          Serial.print("Droping measure #"); Serial.print(idx); Serial.print(" : "); Serial.print(tmp_distance[idx]); 
+          Serial.print(" for it is > mean ("); Serial.print(us_mean); Serial.print(") * DROP_COEF ("); Serial.print(DROP_COEF); Serial.println(")");
+        }
+      }
+    }   
+    distance[i] /= divider; // Mean of remaining measures
+    
     // Computing weight factor
     // weight=1 if sensor says 40cm (60cm = empty tank) / weight=10 if sensor says 20cm(0.33*60cm = full tank)
     weight_factor = int((float(WEIGHT_FACTOR_FULL_SCALE) / float(DIST_FULL_SCALE)) * (DIST_US_BOTTOM - distance[i])) + MIN_WEIGHT_FACTOR;
@@ -110,7 +160,7 @@ void loop() {
     if (DEBUG){
       Serial.print("US #");
       Serial.print(i);
-      Serial.print("- Distance: ");
+      Serial.print("- Averaged Distance (after eventual measures drop): ");
       Serial.print(distance[i]);
       Serial.print("- Weight factor: ");
       Serial.print(weight_factor);
@@ -120,6 +170,7 @@ void loop() {
     }
   }
   if (DEBUG){
+    Serial.print("weight_factor_sum: ");
     Serial.println(weight_factor_sum);
   }
 
@@ -147,7 +198,10 @@ void loop() {
   if (DEBUG){
     Serial.print("Status : ");
     Serial.println(ret);
+    Serial.println("----------------------------------------\n");
   }
+
   
-  LowPower.sleep(1200000); // in milliseconds <-> 20 mn sleep to save the battery before next measurement
+  delay(10000);
+  //LowPower.sleep(1200000); // in milliseconds <-> 20 mn sleep to save the battery before next measurement
 }
